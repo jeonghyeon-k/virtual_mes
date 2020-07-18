a348bb97b4ba5a502acf282d3c190ce140754b79
hyeonji <hyunji980@naver.com>
2020-07-18 09:55:57 +0900

    -test1

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.use('/status', status);
 
 const port = 3000;
 app.listen(port, function(){
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`app listening at http://localhost:${port}`);
 });
   
 
