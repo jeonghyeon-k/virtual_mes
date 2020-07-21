b65ef9dd09cfb2fda0015d8575265a938fece864
김정현 <rlawjd147@gmail.com>
2020-07-21 15:41:46 +0900

    - 공장시스템

--- app.js
+++ app.js
@@ -1,48 +1,3 @@
-// var createError = require('http-errors');
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
-// var app = express();
-
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-// module.exports = app;
-
-
-
-
 // Node.JS 내외부 모듈추출
 const   express = require('express');
 const   cookieParser = require('cookie-parser');
@@ -56,6 +11,7 @@ const   app = express();
 const   stage = require('./routes/stage');
 const   orders = require('./routes/orders');
 const   status = require('./routes/status');
+const   system = require('./routes/system');
 
 // 실행환경 설정부분
 app.set('views', path.join(__dirname, 'views'));  // views경로 설정
@@ -76,10 +32,11 @@ app.use(session({ key: 'sid',
 app.use('/', stage); 
 app.use('/orders', orders);
 app.use('/status', status);
+app.use('/system', system);
 
 
 
-const port = 3000;
+const port = 3001;
 app.listen(port, function(){
   console.log(`Example app listening at http://localhost:${port}`);
 });
